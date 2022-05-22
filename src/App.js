1076c3cff900db187474e10974f31715042d1aef
mrralphpierre <mrralphpierre@gmail.com>
2022-05-22 11:49:55 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+import Prompt from './components/Prompt';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>my react app</h1>
-      <p> testing the waters</p>
+      <Prompt/>
     </div>
   );
 }
